c3ddc60ce675e760957bdadb322625e169dba22a
Manuel Palmero <manueligna06@gmail.com>
2025-06-03 20:06:25 -0300

    Refactor la clase TUI: simplificar la gestión de la consola, mejorar la estructura de la interfaz de usuario y actualizar la lógica de navegación entre opciones.

--- app/src/main/java/parcial/util/TUI.java
+++ app/src/main/java/parcial/util/TUI.java
@@ -1,190 +1,170 @@
 package parcial.util;
 
 import java.io.IOException;
+import java.util.Arrays;
 
-import com.googlecode.lanterna.SGR;
-import com.googlecode.lanterna.TextColor;
-import com.googlecode.lanterna.graphics.SimpleTheme;
-import com.googlecode.lanterna.graphics.Theme;
 import com.googlecode.lanterna.gui2.BasicWindow;
 import com.googlecode.lanterna.gui2.Button;
 import com.googlecode.lanterna.gui2.Direction;
 import com.googlecode.lanterna.gui2.EmptySpace;
+import com.googlecode.lanterna.gui2.Label;
 import com.googlecode.lanterna.gui2.LinearLayout;
 import com.googlecode.lanterna.gui2.MultiWindowTextGUI;
 import com.googlecode.lanterna.gui2.Panel;
-import com.googlecode.lanterna.gui2.dialogs.MessageDialog;
 import com.googlecode.lanterna.screen.TerminalScreen;
 import com.googlecode.lanterna.terminal.DefaultTerminalFactory;
 import com.googlecode.lanterna.terminal.Terminal;
 
 public class TUI {
-    public TUI() {
+    Panel espaciador;
+    EmptySpace espacio;
+    Terminal terminal;
+    TerminalScreen screen;
 
+    public TUI() {
+        // Crea la terminal y la pantalla
+        try {
+            terminal = new DefaultTerminalFactory().createTerminal();
+            screen = new TerminalScreen(terminal);
+            screen.startScreen();
+        } catch (IOException e) {
+            System.err.println(e.getMessage());
+        }
+
+        espaciador = new Panel();
+        espaciador.setLayoutManager(new LinearLayout());
+        espacio = new EmptySpace();
+        espaciador.addComponent(espacio);
     }
 
     public void iniciar() {
-        limpiarConsola();
-        // menuInicial();
-        inicio();
+        int seleccion = 1;
+        while (seleccion != 0) {
+            switch (seleccion) {
+                case 0 -> {return;}
+                case 1 -> seleccion = inicio();
+                case 2 -> seleccion = agregarClientes();
+                case 3 -> seleccion = listarClientes();
+                default -> throw new AssertionError();
+            }
+            limpiarConsola();
+        }
     }
 
     private void limpiarConsola() {
         try {
-            final String os = System.getProperty("os.name").toLowerCase();
-            if (os.contains("win")) {
-                new ProcessBuilder("cmd", "/c", "cls").inheritIO().start().waitFor();
-            } else {
-                new ProcessBuilder("clear").inheritIO().start().waitFor();
-            }
-        } catch (IOException | InterruptedException e) {
+            terminal.clearScreen();
+        } catch (IOException e) {
             // Ignorar errores al limpiar la consola
         }
     }
 
-    /**
-     * Inicia la interfaz de usuario basada en texto utilizando la biblioteca
-     * Lanterna.
-     * <p>
-     * Este método limpia la consola y luego crea una terminal y una pantalla
-     * ({@link TerminalScreen}),
-     * inicializando una interfaz gráfica de usuario de múltiples ventanas
-     * ({@link MultiWindowTextGUI}).
-     * Se crea una ventana principal ({@link BasicWindow}) centrada, que contiene un
-     * panel vertical
-     * ({@link Panel} con {@link LinearLayout} y {@link Direction#VERTICAL}) donde
-     * se agregan botones
-     * para listar clientes, agregar clientes y salir.
-     * <p>
-     * Cada botón ejecuta una acción específica al ser presionado:
-     * <ul>
-     * <li>"Listar clientes": Llama al método
-     * {@link #listarClientes(Terminal, TerminalScreen, MultiWindowTextGUI, BasicWindow)}.</li>
-     * <li>"Agregar cliente": Llama al método
-     * {@link #agregarClientes(Terminal, TerminalScreen, MultiWindowTextGUI, BasicWindow)}.</li>
-     * <li>"Salir": Cierra la ventana principal.</li>
-     * </ul>
-     * <p>
-     * El panel inferior contiene el botón de salida y se centra horizontalmente.
-     * Si ocurre un error de entrada/salida al crear la pantalla, se imprime el
-     * mensaje de error en la salida estándar de error.
-     * <p>
-     * Clases utilizadas:
-     * <ul>
-     * <li>{@link Terminal}: Representa la terminal física o virtual.</li>
-     * <li>{@link TerminalScreen}: Maneja la pantalla sobre la terminal.</li>
-     * <li>{@link DefaultTerminalFactory}: Fábrica para crear terminales y
-     * pantallas.</li>
-     * <li>{@link MultiWindowTextGUI}: Administra la interfaz gráfica de usuario
-     * basada en texto con soporte para múltiples ventanas.</li>
-     * <li>{@link BasicWindow}: Ventana básica que puede contener componentes.</li>
-     * <li>{@link Panel}: Contenedor de componentes con soporte para diferentes
-     * diseños.</li>
-     * <li>{@link LinearLayout}: Gestor de diseño que organiza los componentes en
-     * forma lineal (vertical u horizontal).</li>
-     * <li>{@link Button}: Componente interactivo que ejecuta una acción al ser
-     * presionado.</li>
-     * <li>{@link EmptySpace}: Espaciador visual dentro de los paneles.</li>
-     * </ul>
-     */
-    public void inicio() {
-        try {
-            // Crea la terminal y la pantalla
-            Terminal terminal = new DefaultTerminalFactory().createTerminal();
-            TerminalScreen screen = new TerminalScreen(terminal);
-            screen.startScreen();
-            terminal.clearScreen();
-            screen.clear();
+    public int inicio() {
+        /**
+         * Nota sobre el uso de 'final int[] sel = new int[1];':
+         * Se utiliza un arreglo de un solo elemento en lugar de una variable entera
+         * normal porque las variables locales deben ser efectivamente finales
+         * para ser accedidas dentro de las expresiones lambda (como las acciones de los
+         * botones). Al usar un arreglo, se puede modificar su contenido
+         * dentro de las lambdas, permitiendo así comunicar la selección del usuario
+         * fuera del ámbito de la lambda.
+         */
+        final int[] sel = new int[1];
 
-            // PANELES
+        /*
+         * PANELES
+         */
 
-            // Crear lista de opciones
-            Panel panel = new Panel();
-            panel.setLayoutManager(new LinearLayout(Direction.VERTICAL));
-            // Panel para los botones inferiores
-            Panel panelInferior = new Panel();
-            panelInferior.setLayoutManager(new LinearLayout(Direction.HORIZONTAL));
-            panelInferior.setLayoutData(LinearLayout.createLayoutData(LinearLayout.Alignment.Center));
-            // Espaciador
-            Panel espaciador = new Panel();
-            espaciador.setLayoutManager(new LinearLayout());
-            EmptySpace espacio = new EmptySpace();
-            espaciador.addComponent(espacio);
+        // Crear lista de opciones
+        Panel panel = new Panel();
+        panel.setLayoutManager(new LinearLayout(Direction.VERTICAL));
+        // Panel para los botones inferiores
+        Panel panelInferior = new Panel();
+        panelInferior.setLayoutManager(new LinearLayout(Direction.HORIZONTAL));
+        panelInferior.setLayoutData(LinearLayout.createLayoutData(LinearLayout.Alignment.Center));
+        // Espaciador
 
-            // Crear interfaz gráfica basada en texto
-            BasicWindow ventana = new BasicWindow("Administración de Clientes");
-            MultiWindowTextGUI gui = new MultiWindowTextGUI(screen);
+        // Crear interfaz gráfica basada en texto
+        MultiWindowTextGUI gui = new MultiWindowTextGUI(screen);
 
-            ventana.setHints(java.util.Arrays.asList(BasicWindow.Hint.CENTERED));
-            ventana.setTheme(new SimpleTheme(
-                    TextColor.ANSI.BLACK, // foreground
-                    TextColor.ANSI.WHITE, // background
-                    SGR.REVERSE));
-            gui.setTheme(new SimpleTheme(
-                    TextColor.ANSI.WHITE,
-                    TextColor.ANSI.BLACK));
-            // ventana.setTheme(ventanaTheme);
+        BasicWindow ventana = new BasicWindow(" Administración de Clientes ");
+        ventana.setHints(Arrays.asList(BasicWindow.Hint.CENTERED));
 
-            // BOTONES
+        /*
+         * BOTONES
+         */
 
-            Button listarClientes = new Button("Listar clientes", () -> {
-                listarClientes(gui, ventana);
-            });
-            Button agregarCliente = new Button("Agregar cliente", () -> {
-                agregarClientes(gui, ventana);
-            });
+        Button listarClientes = new Button("Listar clientes", () -> {
+            ventana.close();
+            sel[0] = 3;
+        });
+        Button agregarCliente = new Button("Agregar cliente", () -> {
+            ventana.close();
+            sel[0] = 2;
+        });
 
-            Button salir = new Button("Salir", () -> {
-                ventana.close();
-            });
+        Button salir = new Button("Salir", () -> {
+            ventana.close();
+            sel[0] = 0;
+        });
 
-            Button errorButton = new Button("Error", () -> {
-                error(gui, ventana, "Este es un mensaje de error de prueba.");
-            });
-            panel.addComponent(errorButton);
+        // Agregar botones al panel
+        panel.addComponent(listarClientes);
+        panel.addComponent(agregarCliente);
 
-            // Agregar botones al panel
-            panel.addComponent(listarClientes);
-            panel.addComponent(agregarCliente);
-            // Agregar botones al panel inferior
-            panelInferior.addComponent(salir);
-            // Agregar componentes al panel
-            panel.addComponent(espaciador);
-            panel.addComponent(panelInferior);
+        // Agregar botones al panel inferior
+        panelInferior.addComponent(salir);
 
-            // Agregar panel a la ventana
-            ventana.setComponent(panel);
-            gui.addWindowAndWait(ventana);
+        // Agregar componentes al panel
+        panel.addComponent(espaciador);
+        panel.addComponent(panelInferior);
 
-        } catch (IOException e) {
-            System.err.println(e.getMessage());
+        // Agregar panel a la ventana
+        ventana.setComponent(panel);
+        gui.addWindowAndWait(ventana);
 
-        }
+        return sel[0];
     }
 
-    public void listarClientes(MultiWindowTextGUI gui, BasicWindow ventana) {
+    public int listarClientes() {
         // TODO Implementar lógica para listar clientes
-        System.out.println("Listando clientes...");
+        try {
+            limpiarConsola();
+            screen.clear();
+            screen.newTextGraphics().putString(1, 1, "hola");
+            screen.refresh();
+            Thread.sleep(1000); // Espera para que el usuario vea el mensaje
+        } catch (Exception e) {
+            System.err.println(e.getMessage());
+        }
+        return 1;
+
+        
     }
 
-    public void agregarClientes(MultiWindowTextGUI gui, BasicWindow ventana) {
+    public int agregarClientes() {
         // TODO Implementar lógica para agregar un cliente
         System.out.println("Agregando cliente...");
+        return 0;
     }
 
-    public void error(MultiWindowTextGUI gui, BasicWindow ventana, String mensaje) {
-        Theme defaultTheme = gui.getTheme();
-        Theme ventanaTheme = ventana.getTheme();
-        gui.setTheme(new SimpleTheme(
-                com.googlecode.lanterna.TextColor.ANSI.BLACK, // foreground
-                com.googlecode.lanterna.TextColor.ANSI.RED // background
-        ));
-        ventana.setTheme(new SimpleTheme(TextColor.ANSI.BLACK, TextColor.ANSI.BLACK));
+    public void error(MultiWindowTextGUI gui, String mensaje) {
+        Panel errorPanel = new Panel();
+        errorPanel.setLayoutManager(new LinearLayout(Direction.VERTICAL));
+        BasicWindow ventana = new BasicWindow();
+        ventana.setTitle(" ERROR ");
+        ventana.setHints(java.util.Arrays.asList(BasicWindow.Hint.CENTERED));
 
         // Muestra el mensaje de error en un cuadro de diálogo
-        MessageDialog.showMessageDialog(gui, "Error", mensaje);
-        gui.setTheme(defaultTheme); // Reinicia la ventana al tema original
-        ventana.setTheme(ventanaTheme);
-        // TODO Hacer que se ejecute en una pantalla a parte para que no se vea encima de lo otro
+        Label textoError = new Label(mensaje);
+        Button volver = new Button("Volver", () -> {
+            ventana.close();
+        });
+
+        errorPanel.addComponent(textoError);
+        errorPanel.addComponent(volver);
+        ventana.setComponent(errorPanel);
+        gui.addWindowAndWait(ventana);
+
     }
 }
