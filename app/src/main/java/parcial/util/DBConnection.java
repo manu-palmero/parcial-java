7025a3d7567fb125e4a8f6f34ffcf9cf7780207e
Manuel Palmero <manueligna06@gmail.com>
2025-06-04 00:46:13 -0300

    Refactor la clase DBConnection: mejorar el manejo de errores utilizando logger en lugar de System.err y corregir la ruta del archivo de base de datos.

--- app/src/main/java/parcial/util/DBConnection.java
+++ app/src/main/java/parcial/util/DBConnection.java
@@ -5,17 +5,22 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 import parcial.exception.DatabaseException;
 
 public class DBConnection {
     private static final String SEPARATOR = File.separator;
     private static final String LOCATION = System.getProperty("user.dir") + SEPARATOR;
-    private static final String DB_FILE = LOCATION + "";
+    private static final String DB_FILE = LOCATION + "database";
     private static final String URL = "jdbc:h2:" + DB_FILE;
     private static final String USER = "";
     private static final String PASSWORD = "";
+    protected static final Logger logger = LogManager.getLogger();
 
     public static Connection getConnection() {
+
         Connection c = null;
         try {
             c = DriverManager.getConnection(URL, USER, PASSWORD);
@@ -24,11 +29,11 @@ public class DBConnection {
                 throw new DatabaseException("No se pudo conectar a la base de datos");
             }
         } catch (ClassNotFoundException e) {
-            System.err.println("Controlador de base de datos no encontrado: " + e.getMessage());
+            logger.error("Controlador de base de datos no encontrado: " + e.getMessage());
         } catch (DatabaseException e) {
-            System.err.println(e.getMessage());
+            logger.error(e.getMessage());
         } catch (SQLException e) {
-            System.err.println("Error con la base de datos: " + e);
+            logger.error("Error con la base de datos: " + e);
         }
         return c;
     }
